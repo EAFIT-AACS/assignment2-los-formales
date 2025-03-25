00dc8f15a05d5f2cf208170b31b70f3dc00e47ac
Lunes313 <99741938+Lunes313@users.noreply.github.com>
2025-03-25 18:30:46 -0500

    Update Main

--- MAIN_ALGORITHM_LFCO_2025_JR_LR_DA.py
+++ MAIN_ALGORITHM_LFCO_2025_JR_LR_DA.py
@@ -2,18 +2,17 @@ from ALGORITHM_1_LFCO_2025_JR_LR_DA import generate, print_strings
 from ALGORITHM_2_LFCO_2025_JR_LR_DA import print_results
 from ALGORITHM_3_LFCO_2025_JR_LR_DA import print_configurations_table
 
-
-
 def menu():
     strings = []
     while True:
         print("\n=== PDA MENU ===")
-        print("1. Generate strings")
-        print("2. Input strings")
+        print("1. Generate New strings")
+        print("2. Add strings")
         print("3. Validate strings in the PDA")
         print("4. Print tree for the strings accepted by the PDA")
         print("5. Clear string list")
-        print("6. Exit")
+        print("6. Print grammar rules")
+        print("7. Exit")
         choice = input("Choose an option: ")
 
         if choice == "1":
@@ -41,8 +40,15 @@ def menu():
             strings = []
             print("String list cleared.")
         elif choice == "6":
+            print("|         |      CFG      |              PDA             |")
+            print("|     I.  |    Ψ -> ωΨΩ   |     (q, ω, Ψ) -> (q, ΨΩ)     |")
+            print("|    II.  |    Ψ -> ω     |     (q, ω, Ψ) -> (q, ε)      |")
+            print("|   III.  |    Ψ -> ε     |     (q, ε, Ψ) -> (q, ε)      |")
+            print("|    IV.  |    Ω -> ω     |     (q, ω, Ω) -> (q, ε)      |")
+        elif choice == "7":
             print("Exiting...")
             break
         else:
             print("Invalid option. Please try again.")
+
 menu()
